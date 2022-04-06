25bec5faf74de2e54fd4a9cdf79126d671dced14
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-04-06 10:27:49 +0200

    Add hash JS and img unsplash

---
+++ review-carousel/script.js
@@ -0,0 +1,30 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Ruby Cox",
+    job: "web designer",
+    img: "https://source.unsplash.com/random/100x100/?person",
+    text: "Outstanding service and fast communication. Thank you very much."
+  },
+  {
+    id: 2,
+    name: "Travis King",
+    job: "Web developper",
+    img: "https://source.unsplash.com/random/100x100/?person",
+    text: "Great result. Thanks."
+  },
+  {
+    id: 3,
+    name: "Rebecca Rice",
+    job: "Web project manager",
+    img: "https://source.unsplash.com/random/100x100/?person",
+    text: "Amazingly outstanding result and quick communication Thanks a bunch!"
+  },
+  {
+    id: 4,
+    name: "Natalia Allen",
+    job: "UX researcher",
+    img: "https://source.unsplash.com/random/100x100/?person",
+    text: "Satisfying outcome! Thanks!"
+  },
+];
