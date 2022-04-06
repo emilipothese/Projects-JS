607a41211558ee213c44a99124886c9088e6449b
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-04-06 10:43:54 +0200

    JS avance

--- review-carousel/script.js
+++ review-carousel/script.js
@@ -28,3 +28,28 @@ const reviews = [
     text: "Satisfying outcome! Thanks!"
   },
 ];
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson(currentItem);
+});
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
