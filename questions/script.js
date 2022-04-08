9bfea07e43122997c3d76dcf44a6d22af6ccfa06
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-04-08 10:06:31 +0200

    JS 2 versions

--- questions/script.js
+++ questions/script.js
@@ -1,8 +1,27 @@
-const btns = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
 
-btns.forEach(function(btn) {
-  btn.addEventListener("click", function(e) {
-    const question = e.currentTarget.parentElement.parentElement;
+questions.forEach(function(question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function() {
+    // Close element if open and another element clicked
+    questions.forEach(function(item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    
     question.classList.toggle("show-text");
   });
 });
+
+// Other option
+
+// const btns = document.querySelectorAll(".question-btn");
+//
+// btns.forEach(function(btn) {
+//   btn.addEventListener("click", function(e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
