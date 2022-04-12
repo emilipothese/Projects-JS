514c85e2393165d47d3b52c8e6f834a9d5eede73
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-04-12 15:18:58 +0200

    First video OK

---
+++ first-api/script.js
@@ -0,0 +1,12 @@
+const obj1 = {
+  "first" : "Laurence",
+  "last" : "D'Arabie",
+  "num" : 100,
+  "nu" : null,
+  "arr" : [],
+  "obj" : {}
+};
+
+console.log(obj1);
+console.log(obj1.first);
+console.log(obj1["first"]);
