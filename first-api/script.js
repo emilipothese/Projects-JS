dcf28adcb9cf6501f2bce9f722f9a9089b65997e
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-04-12 15:26:29 +0200

    Arrays exemple OK

--- first-api/script.js
+++ first-api/script.js
@@ -1,3 +1,4 @@
+// Object
 const obj1 = {
   "first" : "Laurence",
   "last" : "D'Arabie",
@@ -10,3 +11,13 @@ const obj1 = {
 console.log(obj1);
 console.log(obj1.first);
 console.log(obj1["first"]);
+
+// Array
+const arr = ["Laurence", 100, true, null, {
+  "first" : "Laurence",
+  "last" : "D'arabie"
+}, "Laurence", "Laurence", "Laurence"];
+
+console.log(arr);
+console.log(arr[2]);
+console.log(arr[4].last);
