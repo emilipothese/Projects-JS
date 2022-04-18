dd6f7133e030a19866beefb14f9a78d0e1dff621
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-04-18 10:55:42 +0200

    Ex AJAX done

--- first-api/ajax.js
+++ first-api/ajax.js
@@ -1,17 +1,42 @@
-const btn = document.querySelector(".btn");
-const output = document.querySelector(".output");
+const btn = document.querySelector('.btn');
+const btn1 = document.querySelector('.btn1');
+const output = document.querySelector('.output');
 let counter = 0;
+const url = 'json.json';
 
-output.innerHtml = "<h1>Hello World</h1>";
+console.log(btn);
 
-btn.onclick = (e) => {
-  console.log(e.target);
-}
+output.innerHTML = '<h1>Hello World</h1>';
 
-btn.onclick = adder;
+document.addEventListener('DOMContentLoaded', (e) => {
+   getValues();
+ })
 
-function adder(e) {
-  console.log(e.target);
-  counter++;
-  output.innerHtml = `<h2>Counter : ${counter}</h2>`;
-}
+ function getValues() {
+   fetch(url)
+   .then(rep => rep.json())
+   .then(data => {
+    console.log(data);
+    counter = data.counter;
+    updater();
+ }) }
+
+ //output.textContent = '<h1>Hello World</h1>';
+
+ btn.onclick = (e) => { console.log(e.target);}
+ btn.onclick = adder;
+ btn1.onclick = resetCounter;
+
+ function updater() {
+   output.innerHTML = `<h2>Counter : ${counter}</h2>`;
+ }
+
+ function adder(e) {
+    console.log(e.target);
+    counter++;
+    updater();
+  }
+
+  function resetCounter(e) {
+    getValues();
+  }
