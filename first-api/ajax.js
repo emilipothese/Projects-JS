43f8283784ef220c1c2dbd7b7c2fcb2dad279170
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-04-18 10:48:06 +0200

    HTML and JSON OK

--- first-api/ajax.js
+++ first-api/ajax.js
@@ -1,4 +1,17 @@
 const btn = document.querySelector(".btn");
 const output = document.querySelector(".output");
+let counter = 0;
 
-output.innerHtml =
+output.innerHtml = "<h1>Hello World</h1>";
+
+btn.onclick = (e) => {
+  console.log(e.target);
+}
+
+btn.onclick = adder;
+
+function adder(e) {
+  console.log(e.target);
+  counter++;
+  output.innerHtml = `<h2>Counter : ${counter}</h2>`;
+}
